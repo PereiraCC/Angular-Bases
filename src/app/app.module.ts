c21d63d32903f195f60aa1f426944fc5e554c3d8
Carlos Pereira <carloscoto0103@gmail.com>
2024-11-05 17:18:39 -0600

    Primera bases de Angular, Counter, Heroes modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { CounterModule } from './counter/counter.module';
+
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
+import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
